240bc58b3bdcfd648b258fd3ad7551a68aa165eb
Jad Taha <jadtaha.de@gmail.com>
2022-09-30 09:29:59 +0200

    thunk & dispatchers --version01.00.15

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './state/index';
 
 function App() {
   const account = useSelector((state) => state.account);
+  const dispatch = useDispatch();
 
-  console.log(account);
+  const { depositMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>{account}</h1>
+      <button onClick={() => depositMoney(1000)}>Deposit</button>
+      <button onClick={() => withdrawMoney(1000)}>Withdraw</button>
+    </div>
+  );
 }
 
 export default App;
